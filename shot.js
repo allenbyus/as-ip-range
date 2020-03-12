fd8f3d52417e47ecbe2a2885780c66cda65e5757
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-03-12 19:10:32 +0800

    Update shot.js

--- shot.js
+++ shot.js
@@ -1,3 +1,4 @@
-var ip='';
 //https://bgp.he.net/AS16509#_prefixes
-$('tr a').each(function(){ip+=$(this).text()+"\n"})
+var ip='';$('tr a').each(function(){ip+=$(this).text()+"\n"});ip
+//https://ipinfo.io/as9808
+var ip='';$('tbody tr td a').each(function(){ip+=$(this).text()+"\n"});ip
